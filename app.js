6875d109f45a058b8bc0dd8f215609958c99226a
Syprose Gwako <syprosegwako@gmail.com>
2020-06-17 23:06:49 +0300

    main

---
+++ app.js
@@ -0,0 +1,26 @@
+const ObjectsToCsv = require('objects-to-csv');
+const moment = require('moment')
+
+// Sample data - two columns, three rows:
+const data = [
+    { uID: '123', name: 'Syprose Gwako', timestamp: "2020-06-11T15:37:44.000Z" },
+    { uID: '456', name: 'Victor Chelule', timestamp: 1382086394000 },
+    { uID: '789', name: 'Lyanne Mapani', timestamp: "2016-03-12T00:00:00-06:00" },
+];
+const preprocessedData = data.map((item, i) => ({
+    index: i,
+    uID: item.uID,
+    name: item.name,
+    timestamp: moment(item.timestamp).format('MMMM Do YYYY, h:mm:ss a')
+}));
+
+// If you use "await", code must be inside an asynchronous function:
+(async () => {
+    const csv = new ObjectsToCsv(preprocessedData);
+
+    // Save to file:
+    await csv.toDisk('./destination.csv');
+
+    // Return the CSV file as string:
+    console.log(await csv.toString());
+})();
