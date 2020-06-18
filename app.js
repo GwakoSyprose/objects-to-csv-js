0051ebb4e257bbcab6edd02c80c54b7f61d2e8d2
Syprose Gwako <syprosegwako@gmail.com>
2020-06-18 11:58:20 +0300

    updates

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const ObjectsToCsv = require('objects-to-csv');
 const moment = require('moment')
 
-// Sample data - two columns, three rows:
+// Sample data 
 const data = [
     { uID: '123', name: 'Syprose Gwako', timestamp: "2020-06-11T15:37:44.000Z" },
     { uID: '456', name: 'Victor Chelule', timestamp: 1382086394000 },
     { uID: '789', name: 'Lyanne Mapani', timestamp: "2016-03-12T00:00:00-06:00" },
 ];
+//mapping to manipulate data
 const preprocessedData = data.map((item, i) => ({
     index: i,
     uID: item.uID,
